007ad2c7e0b6a1dc9c3896f6869419a769d4b6f2
Shmalebx9 <funkusucker3@gmail.com>
2021-10-03 22:43:03 -0600

    v1 commit

--- patch.sh
+++ patch.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
 Cbchange(){
-for rom in bin/*.rom ; do
+for rom in *.rom ; do
 	echo "adding $1 to $rom"
-	cbfstool $rom remove $1 -n $1 2> /dev/null
-	cbfstool $rom add -f $1 -n $1 -t raw
+	cbfstool $rom remove -n $1
+ cbfstool $rom add -f $1 -n $1 -t raw || echo "error, could not add $1 to $rom"
 
 done
 }
 
 dir=$PWD
 type=$1
-cp roms/*.rom bin/
+cp roms/* "$type"
+cd "$type"
 
-for file in "$type/"* ; do
- Cbchange $file 
+Cbchange background.*
+Cbchange grub.cfg
+Cbchange theme.txt
+Cbchange font.pf2
+
+if [ "$type" = "custom" ] ; then
+ for rom in *.rom ; do
+ echo "disabling beeps in $rom"
+ nvramtool -C $rom -w power\_management\_beeps=Disable 
+ nvramtool -C $rom -w low\_battery\_beep=Disable
 done
+fi
+
+mv *.rom "$dir/bin"
+
+cd "$dir"
